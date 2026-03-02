b40059cd504ea4462f0606100dcdad4fff566720
Orchid <lokipokes@hotmail.com>
2026-03-02 00:14:51 +0000

    just testing, changed the repo name

---
+++ main.py
@@ -0,0 +1,9 @@
+from wordfreq import get_frequency_dict
+
+# This only supports english currently, this is a list of 'common' english words
+enWords = get_frequency_dict('en', wordlist='best')
+
+ceaserText = input("Ceaser cipher text: ")
+
+
+
